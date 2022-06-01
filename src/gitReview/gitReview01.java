68b9511fa88d517b996314d1158785e14f7c0b1a
Jason Kaminsky <jasonkaminsky@Jasons-iMac.local>
2022-05-31 20:35:55 -0500

    first commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,14 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("I am code");
+		System.out.println("I am more code");
+		
+
+	}
+
+}
